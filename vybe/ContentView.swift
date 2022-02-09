cf28c4648a950b620516180d77d4d448f07fad63
Ishmeet Sethi <sethi.ishmeet@gmail.com>
2022-02-09 12:10:12 -0500

    Initial Commit

---
+++ vybe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vybe
+//
+//  Created by Ishmeet Singh Sethi on 2022-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vybe/vybeApp.swift
@@ -0,0 +1,17 @@
+//
+//  vybeApp.swift
+//  vybe
+//
+//  Created by Ishmeet Singh Sethi on 2022-02-09.
+//
+
+import SwiftUI
+
+@main
+struct vybeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
